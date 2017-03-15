f2a66c56e3dd2caf451a256ace36650e1e80b02a
root <root@vps375379.ovh.net>
2017-03-15 12:33:33 +0100

    Mise a jour : Communication avec la DB fonctionnelle

---
+++ BackupWeb15-03-17/etc/apache2/sites-available/b2b.wt11.ephec-ti.be
@@ -0,0 +1,7 @@
+<VirtualHost *:80>
+     ServerAdmin webmaster@localhost
+     ServerName b2b.wt11.ephec-ti.be
+     DocumentRoot /var/www/html/b2b.wt11.ephec-ti.be
+     ErrorLog /error.log
+     CustomLog /access.log combined
+</VirtualHost>
--- docker-images/content/b2b.wt11.ephec-ti.be/index.php
+++ docker-images/content/b2b.wt11.ephec-ti.be/index.php
@@ -1,1 +1,14 @@
-<?php try { $db = new PDO("mysql:host=172.17.0.5;port=3306;dbname=demo","root","admin"); echo "=================   La connexion a bien commence   ================="."<br>"; }catch(PDOException  $e ) { echo "Error: " . $e; }
+<?php
+	try {
+		$db = new PDO("mysql:host=172.17.0.4;
+				port=3306;dbname=demo","root","admin");
+		 echo "=================   La connexion a bien commence   ================="."<br>";
+	}catch(PDOException  $e ) {
+		echo "Error: " . $e;
+	}
+
+	$sql = "SELECT * FROM a";
+	$requete = $db->query($sql);
+	foreach($requete as $row) {
+		echo  $row->testCol1 . "Ca fonctionne !!";
+	}
